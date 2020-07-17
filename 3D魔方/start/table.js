7d6c86599aa7437803bf9f694c62a14040c9a28b
liuming <962398309@qq.com>
2020-07-17 09:43:10 +0800

    3D魔方

---
+++ 3D魔方/start/table.js
@@ -0,0 +1,52 @@
+var data = [{
+	name: '大气环境',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (1).png'
+}, {
+	name: '固废危废',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (2).png'
+}, {
+	name: '行政监督',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (3).png'
+}, {
+	name: '监察执法',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (4).png'
+}, {
+	name: '目标考核',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (5).png'
+}, {
+	name: '陕西辐安',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (6).png'
+}, {
+	name: '生态督察',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (7).png'
+}, {
+	name: '水环境',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (8).png'
+}, {
+	name: '统一运维',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (9).png'
+}, {
+	name: '土壤环境',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (10).png'
+}, {
+	name: '一张图',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (11).png'
+}, {
+	name: '自然生态',
+	url: 'https://www.baidu.com',
+	icon: './images/icon (12).png'
+}]
+export {
+	data
+}
